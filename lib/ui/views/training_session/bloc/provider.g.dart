3a73609fc2ad9cf8bcd1095527c561baa841c265
redDwarf03 <sylvain@archethic.net>
2025-09-19 11:35:29 +0200

    Fix merge conflicts in provider.g.dart and update training session notifier hash.

--- lib/ui/views/training_session/bloc/provider.g.dart
+++ lib/ui/views/training_session/bloc/provider.g.dart
@@ -7,32 +7,7 @@ part of 'provider.dart';
 // **************************************************************************
 
 String _$trainingSessionNotifierHash() =>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
     r'3a83c34b6c9d179967b6ef365da15032ac2f9281';
-=======
-=======
->>>>>>> 36f693d (fix rebase)
-<<<<<<< HEAD
-    r'f942ae55a08cafe8b77caee70237bc0d3f8537e5';
-=======
-<<<<<<< HEAD
-    r'33816e13afb7e9bbe1f80c4b34809635ef9cfa0f';
-=======
-    r'cd1ea3d5266d9409058034f6b046f0b75ecf7909';
->>>>>>> 6524c3b (Rebuild .g)
->>>>>>> ee76d1d (Rebuild .g)
-<<<<<<< HEAD
->>>>>>> 17bf8c3 (Rebuild .g)
-=======
-=======
-    r'a83d1a2bc17e31327290428685d404a07cdc5b47';
->>>>>>> e87ab2a (fix rebase)
->>>>>>> 36f693d (fix rebase)
-=======
-    r'f942ae55a08cafe8b77caee70237bc0d3f8537e5';
->>>>>>> 231feee (fix g)
 
 /// See also [TrainingSessionNotifier].
 @ProviderFor(TrainingSessionNotifier)
