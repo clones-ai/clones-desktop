clones-sol/desktop

feat: :sparkles: Enhance demo detail and leaderboard views with fullscreen functionality and UI improvements for video previews and leaderboard displays.

lib/ui/views/leaderboards/layouts/components/top_forges.dart
@@ -42,64 +42,45 @@ class _TopForgesState extends ConsumerState<TopForges> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 25,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildLeaderboardTable(context),
-            ],
-          ),
-        ),
-        if (widget.showTitle)
-          Positioned(
-            top: 8,
-            right: 20,
-            child: Row(
-              children: [
-                const Text(
-                  'Top Forges',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: VMColors.primary,
+    return CardWidget(
+      variant: CardVariant.black,
+      padding: CardPadding.none,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (widget.showTitle)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 16, 8, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Top Forges',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: VMColors.primary,
+                    ),
                   ),
-                ),
-                const SizedBox(width: 12),
-                IconButton(
-                  icon:
-                      const Icon(Icons.open_in_full, color: VMColors.secondary),
-                  onPressed: widget.onExpand,
-                ),
-              ],
-            ),
-          ),
-      ],
-    );
-  }
-
-  Widget _buildLeaderboardTable(BuildContext context) {
-    return Column(
-      children: [
-        CardWidget(
-          variant: CardVariant.black,
-          padding: CardPadding.none,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _buildTableHeader(context),
-              SizedBox(
-                height: widget.listHeight ?? 300,
-                child: _buildForgesList(),
+                  if (widget.onExpand != null)
+                    IconButton(
+                      icon: const Icon(
+                        Icons.fullscreen,
+                        color: VMColors.secondary,
+                      ),
+                      onPressed: widget.onExpand,
+                      tooltip: 'Fullscreen',
+                    ),
+                ],
               ),
-            ],
+            ),
+          _buildTableHeader(context),
+          SizedBox(
+            height: widget.listHeight ?? 300,
+            child: _buildForgesList(),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 

lib/ui/views/leaderboards/layouts/components/top_workers.dart
@@ -39,66 +39,45 @@ class _TopWorkersState extends ConsumerState<TopWorkers> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildLeaderboardTable(context),
-            ],
-          ),
-        ),
-        if (widget.showTitle)
-          Positioned(
-            top: 8,
-            right: 20,
-            child: Row(
-              children: [
-                const Text(
-                  'Top Workers',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: VMColors.primary,
-                  ),
-                ),
-                if (widget.onExpand != null) ...[
-                  const SizedBox(width: 12),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.open_in_full,
-                      color: VMColors.secondary,
+    return CardWidget(
+      variant: CardVariant.black,
+      padding: CardPadding.none,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (widget.showTitle)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 16, 8, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Top Workers',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: VMColors.primary,
                     ),
-                    onPressed: widget.onExpand,
                   ),
+                  if (widget.onExpand != null)
+                    IconButton(
+                      icon: const Icon(
+                        Icons.fullscreen,
+                        color: VMColors.secondary,
+                      ),
+                      onPressed: widget.onExpand,
+                      tooltip: 'Fullscreen',
+                    ),
                 ],
-              ],
-            ),
-          ),
-      ],
-    );
-  }
-
-  Widget _buildLeaderboardTable(BuildContext context) {
-    return Column(
-      children: [
-        CardWidget(
-          variant: CardVariant.black,
-          padding: CardPadding.none,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _buildTableHeader(context),
-              SizedBox(
-                height: widget.listHeight ?? 300,
-                child: _buildWorkersList(),
               ),
-            ],
+            ),
+          _buildTableHeader(context),
+          SizedBox(
+            height: widget.listHeight ?? 300,
+            child: _buildWorkersList(),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 

clones-ai/clones-desktop

lib/ui/views/leaderboards/layouts/components/top_forges.dart
@@ -42,66 +42,45 @@ class _TopForgesState extends ConsumerState<TopForges> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(
-            top: 25,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildLeaderboardTable(context),
-            ],
-          ),
-        ),
-        if (widget.showTitle)
-          Positioned(
-            top: 8,
-            right: 20,
-            child: Row(
-              children: [
-                const Text(
-                  'Top Forges',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: ClonesColors.primary,
+    return CardWidget(
+      variant: CardVariant.black,
+      padding: CardPadding.none,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (widget.showTitle)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 16, 8, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Top Forges',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: ClonesColors.primary,
+                    ),
                   ),
-                ),
-                const SizedBox(width: 12),
-                IconButton(
-                  icon: const Icon(
-                    Icons.open_in_full,
-                    color: ClonesColors.secondary,
-                  ),
-                  onPressed: widget.onExpand,
-                ),
-              ],
-            ),
-          ),
-      ],
-    );
-  }
-
-  Widget _buildLeaderboardTable(BuildContext context) {
-    return Column(
-      children: [
-        CardWidget(
-          variant: CardVariant.black,
-          padding: CardPadding.none,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _buildTableHeader(context),
-              SizedBox(
-                height: widget.listHeight ?? 300,
-                child: _buildForgesList(),
+                  if (widget.onExpand != null)
+                    IconButton(
+                      icon: const Icon(
+                        Icons.fullscreen,
+                        color: ClonesColors.secondary,
+                      ),
+                      onPressed: widget.onExpand,
+                      tooltip: 'Fullscreen',
+                    ),
+                ],
               ),
-            ],
+            ),
+          _buildTableHeader(context),
+          SizedBox(
+            height: widget.listHeight ?? 300,
+            child: _buildForgesList(),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 

lib/ui/views/leaderboards/layouts/components/top_workers.dart
@@ -40,66 +40,45 @@ class _TopWorkersState extends ConsumerState<TopWorkers> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 25),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              _buildLeaderboardTable(context),
-            ],
-          ),
-        ),
-        if (widget.showTitle)
-          Positioned(
-            top: 8,
-            right: 20,
-            child: Row(
-              children: [
-                const Text(
-                  'Top Workers',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: ClonesColors.primary,
-                  ),
-                ),
-                if (widget.onExpand != null) ...[
-                  const SizedBox(width: 12),
-                  IconButton(
-                    icon: const Icon(
-                      Icons.open_in_full,
-                      color: ClonesColors.secondary,
+    return CardWidget(
+      variant: CardVariant.black,
+      padding: CardPadding.none,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (widget.showTitle)
+            Padding(
+              padding: const EdgeInsets.fromLTRB(20, 16, 8, 0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Top Workers',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: ClonesColors.primary,
                     ),
-                    onPressed: widget.onExpand,
                   ),
+                  if (widget.onExpand != null)
+                    IconButton(
+                      icon: const Icon(
+                        Icons.fullscreen,
+                        color: ClonesColors.secondary,
+                      ),
+                      onPressed: widget.onExpand,
+                      tooltip: 'Fullscreen',
+                    ),
                 ],
-              ],
-            ),
-          ),
-      ],
-    );
-  }
-
-  Widget _buildLeaderboardTable(BuildContext context) {
-    return Column(
-      children: [
-        CardWidget(
-          variant: CardVariant.black,
-          padding: CardPadding.none,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              _buildTableHeader(context),
-              SizedBox(
-                height: widget.listHeight ?? 300,
-                child: _buildWorkersList(),
               ),
-            ],
+            ),
+          _buildTableHeader(context),
+          SizedBox(
+            height: widget.listHeight ?? 300,
+            child: _buildWorkersList(),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 